bb4d13130a406a44694476b0b437c0ae2cd262ef
MaxInzunza <max.inzunza@gmail.com>
2026-05-26 15:33:57 -0400

    Update Parte2.py

--- Parte2.py
+++ Parte2.py
@@ -55,71 +55,66 @@ def integrar_rk4_morse(x0, p0, t_max, dt, D_e, a, x_e, masa=1):
 
     return t, x, p, E
 
-
-# ==========================================================
-# Parametros
-# ==========================================================
-
-D_e = 8
-a = 0.9
-x_e = 0
-masa = 1
-
-# Condiciones iniciales
-x0 = 1
-p0 = 0
-
-# Paso y tmax
-dt = 0.01
-t_max = 40
-
-
-# ==========================================================
-# RK4
-# ==========================================================
-
-t, x, p, E = integrar_rk4_morse(x0, p0, t_max, dt, D_e, a, x_e, masa=masa)
-
-
-# ==========================================================
-# Grafico: Potencial de morse y energía
-# ==========================================================
-x_pot = np.linspace(-1.5, 5, 1200)
-V_pot = potencial_morse(x_pot, D_e, a, x_e, desplazado=True)
-
-plt.figure(figsize=(9, 6))
-plt.plot(x_pot, V_pot, label="Potencial de Morse", linewidth=2)
-plt.axhline(E[0], color="red", linestyle="--", linewidth=1.5, label=f"Energía total = {E[0]:.3f}")
-plt.axhline(0, color="black", linestyle=":", linewidth=1.2, label="Límite de disociación")
-plt.xlabel("x")
-plt.ylabel("V(x)")
-plt.title("Pozo de Morse y energía clásica de la trayectoria")
-plt.xlim(-1.2, 4.5)
-plt.ylim(-9, 3)
-plt.grid(True)
-plt.legend()
-plt.show()
-
-
-# ==========================================================
-# Grafico: x vs t
-# ==========================================================
-plt.figure(figsize=(9, 5))
-plt.plot(t, x, linewidth=1.8)
-plt.xlabel("t")
-plt.ylabel("x(t)")
-plt.title("Posición en función del tiempo")
-plt.grid(True)
-plt.show()
-
-
-# ==========================================================
-# Grafico: p vs t
-# ==========================================================
-plt.figure(figsize=(9, 5))
-plt.plot(t, p, linewidth=1.8)
-plt.xlabel("t")
-plt.ylabel("p(t)")
-plt.title("Momento en función del tiempo")
-plt.grid(True)
-plt.show()
+# Para poder utilizar las funciones de arriba en otros archivos .py (Parte 4)
+if __name__ == "__main__":
+    # Parametros
+    D_e = 8
+    a = 0.9
+    x_e = 0
+    masa = 1
+    
+    # Condiciones iniciales
+    x0 = 1
+    p0 = 0
+    
+    # Paso y tmax
+    dt = 0.01
+    t_max = 40
+    
+    
+    # RK4
+    t, x, p, E = integrar_rk4_morse(x0, p0, t_max, dt, D_e, a, x_e, masa=masa)
+    
+    
+    # ==========================================================
+    # Grafico: Potencial de morse y energía
+    # ==========================================================
+    x_pot = np.linspace(-1.5, 5, 1200)
+    V_pot = potencial_morse(x_pot, D_e, a, x_e, desplazado=True)
+    
+    plt.figure(figsize=(9, 6))
+    plt.plot(x_pot, V_pot, label="Potencial de Morse", linewidth=2)
+    plt.axhline(E[0], color="red", linestyle="--", linewidth=1.5, label=f"Energía total = {E[0]:.3f}")
+    plt.axhline(0, color="black", linestyle=":", linewidth=1.2, label="Límite de disociación")
+    plt.xlabel("x")
+    plt.ylabel("V(x)")
+    plt.title("Pozo de Morse y energía clásica de la trayectoria")
+    plt.xlim(-1.2, 4.5)
+    plt.ylim(-9, 3)
+    plt.grid(True)
+    plt.legend()
+    plt.show()
+    
+    
+    # ==========================================================
+    # Grafico: x vs t
+    # ==========================================================
+    plt.figure(figsize=(9, 5))
+    plt.plot(t, x, linewidth=1.8)
+    plt.xlabel("t")
+    plt.ylabel("x(t)")
+    plt.title("Posición en función del tiempo")
+    plt.grid(True)
+    plt.show()
+    
+    
+    # ==========================================================
+    # Grafico: p vs t
+    # ==========================================================
+    plt.figure(figsize=(9, 5))
+    plt.plot(t, p, linewidth=1.8)
+    plt.xlabel("t")
+    plt.ylabel("p(t)")
+    plt.title("Momento en función del tiempo")
+    plt.grid(True)
+    plt.show()
